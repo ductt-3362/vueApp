e704c5ffdb4eb95ec0226a9bd08ea89415772633
tran thang duc <tran.thang.duc@sun-asterisk.com>
2023-09-13 13:44:58 +0700

    ci: fix yml

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
+/* eslint-disable no-undef */
  // tailwind.config.js
- // eslint-disable-next-line no-undef
  module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
    darkMode: false, // or 'media' or 'class'
