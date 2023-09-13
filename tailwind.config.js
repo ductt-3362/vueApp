940fcc079a4ad894ae1772e57c015f9a3045191d
tran thang duc <tran.thang.duc@sun-asterisk.com>
2023-09-13 11:28:01 +0700

    ci: update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,4 @@
- // tailwind.config.js
- module.exports = {
-  purge: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-   darkMode: false, // or 'media' or 'class'
-   theme: {
-     extend: {},
-   },
-   variants: {
-     extend: {},
-   },
-   plugins: [],
- }
+export const plugins = {
+  tailwindcss: {},
+  autoprefixer: {},
+};
